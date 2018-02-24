84c0e228fa3f7a24f85923e1579fc99b8b250f03
Chris Anderson from MET5Workstation <cnanderson@lbl.gov>
2018-02-24 15:14:38 -0800

    new config

--- src/+bl12014/+device/GetSetNumberFromExitSlitObject.m
+++ src/+bl12014/+device/GetSetNumberFromExitSlitObject.m
@@ -23,17 +23,31 @@ classdef GetSetNumberFromExitSlitObject < mic.interface.device.GetSetNumber
         end
         
         function set(this, dVal)
-            fprintf('bl12014.device.GetSetNumberFromExitSlitObject.set(%1.3f)\n', dVal);
+            % fprintf('bl12014.device.GetSetNumberFromExitSlitObject.set(%1.3f)\n', dVal);
             [e,estr] = this.comm.setSlitGap(dVal);
             
         end
         
         function l = isReady(this)
+            this.comm.CLstatus;
+            %{
+            [pos4,e,estr]=this.comm.getPosRaw(4);
+            [pos5,e,estr]=this.comm.getPosRaw(5);
+            [pos6,e,estr]=this.comm.getPosRaw(6);
+            [pos7,e,estr]=this.comm.getPosRaw(7);
+            fprintf('raw pos (4, 5, 6, 7) = (%1.0f, %1.0f, %1.0f, %1.0f)\n', ...
+                pos4, ...
+                pos5, ...
+                pos6, ...
+                pos7 ...
+            );
+            %}
             l = this.comm.CLstatus == 0;
         end
         
         function stop(this)
            % [e,estr]= this.comm.stopAll();
+            
            [e,estr]= this.comm.abortAll();
         end
         
