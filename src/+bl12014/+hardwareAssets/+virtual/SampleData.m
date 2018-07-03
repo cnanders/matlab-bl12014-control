11df3ea08846f858cdc6dc2e191b61a36ebc876a
Chris Anderson <cnanders@gmail.com>
2018-07-03 13:08:02 -0700

    updating SampleData to return column list as this is what the Java SampleData returns

--- src/+bl12014/+hardwareAssets/+virtual/SampleData.m
+++ src/+bl12014/+hardwareAssets/+virtual/SampleData.m
@@ -17,20 +17,20 @@ classdef SampleData < handle
     methods
         
         function this = SampleData(varargin)
-            this.u32DmiData = randi(200, 1, 4) - 100;
-            this.u32HsData = randi(50e3, 1, 24) + 425e3;
+            this.u32DmiData = randi(200, 4, 1) - 100;
+            this.u32HsData = randi(50e3, 24, 1) + 425e3;
         end
         
         % Returns {uint32 1 x 4} of most recent HS / DMI 1 kHz data
         % @param {int32 1x1} i32Samples - number of samples, max 10k.
-        % @return {uint32 1x4} - four raw DMI values of a 1 ms instant of time
+        % @return {uint32 4x1} - four raw DMI values of a 1 ms instant of time
         function u32 = getDmiData(this)
             u32 = this.u32DmiData;
         end
         
         % Returns ADC counts of 12 channels for two 500 us integration
         % windows for a total of 24 values
-        % @return {uint32 x 24} - of one ms worth of acquisition time.
+        % @return {uint32 24x1} - of one ms worth of acquisition time.
         function u32 = getHsData(this)
             u32 = this.u32HsData;
         end
