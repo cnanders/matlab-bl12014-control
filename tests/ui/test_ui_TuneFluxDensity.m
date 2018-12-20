5b94f510bf24734c65bed8c630a46d922ca4bff9
Chris Anderson <cnanders@gmail.com>
2018-12-20 11:18:29 -0800

    built tuneFluxDensity tab

---
+++ tests/ui/test_ui_TuneFluxDensity.m
@@ -0,0 +1,52 @@
+try
+    purge
+end
+
+[cDirThis, cName, cExt] = fileparts(mfilename('fullpath'));
+
+% bl12014 pkg
+cDirBl12014 = fullfile(cDirThis, '..', '..', 'src');
+addpath(genpath(cDirBl12014));
+
+% dependencies
+cDirVendor = fullfile(cDirThis, '..', '..', 'vendor');
+
+cDirMic = fullfile(cDirVendor, 'github', 'cnanders', 'matlab-instrument-control', 'src');
+addpath(genpath(cDirMic));
+
+cDirRigol = fullfile(cDirVendor, 'github', 'cnanders', 'matlab-rigol-dg1000z', 'src');
+addpath(cDirRigol)
+
+
+hardware = bl12014.Hardware();
+waferExposureHistory = bl12014.WaferExposureHistory();
+waferExposureHistory.addFakeExposures();
+
+
+clock = mic.Clock('Master');
+
+ui = bl12014.ui.TuneFluxDensity(...
+    'clock', clock, ...
+    'waferExposureHistory', waferExposureHistory ...
+);
+
+
+dWidth = 1650;
+dHeight = 900;
+
+dScreenSize = get(0, 'ScreenSize');
+h = figure(...
+    'Position', [ ...
+        (dScreenSize(3) - dWidth)/2 ...
+        (dScreenSize(4) - dHeight)/2 ...
+        dWidth ...
+        dHeight ...
+    ] ...
+);
+
+ui.build(h, 10, 10);
+
+
+
+ 
+
