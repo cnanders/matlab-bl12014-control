cc62feaeed2791b2b13318e4300b07e4507b49fb
Chris Anderson <cnanders@gmail.com>
2019-05-21 15:24:17 -0700

    rfxy now centers on every exposure. issue commandcode=6 before every wm4.  added a wafer stage state checker to ma

--- tests/ui/test_ui_MA.m
+++ tests/ui/test_ui_MA.m
@@ -1,24 +1,19 @@
+try
+    purge
+catch mE
+end
+
 [cDirThis, cName, cExt] = fileparts(mfilename('fullpath'));
-
-% bl12014 src
-cDirBl12014 = fullfile(cDirThis, '..', '..', 'src');
-addpath(genpath(cDirBl12014));
-
-% dependencies
-cDirVendor = fullfile(cDirThis, '..', '..', 'vendor');
-
-addpath(genpath(fullfile(cDirVendor, 'github', 'cnanders', 'matlab-instrument-control', 'src')));
-addpath(genpath(fullfile(cDirVendor, 'github', 'cnanders', 'matlab-quasar', 'src')));
-addpath(genpath(fullfile(cDirVendor, 'github', 'cnanders', 'matlab-gridded-pupil-fill', 'src')));
-addpath(genpath(fullfile(cDirVendor, 'github', 'cnanders', 'matlab-pupil-fill-generator', 'src')));
-addpath(genpath(fullfile(cDirVendor, 'github', 'cnanders', 'matlab-npoint-lc400-ui', 'src')));
-addpath(genpath(fullfile(cDirVendor, 'github', 'cnanders', 'matlab-npoint-lc400', 'src')));
-purge
+addpath(genpath(fullfile(cDirThis, '..', '..', 'src')));
+addpath(genpath(fullfile(cDirThis, '..', '..', 'mpm-packages')));
 
 clock = mic.Clock('Master');
 uiClock = mic.ui.Clock(clock);
+hardware = bl12014.Hardware('clock', clock);
+
 
 ui = bl12014.ui.MA(...
+    'hardware', hardware, ...
     'uiClock', uiClock, ...
     'clock', clock ...
 );
