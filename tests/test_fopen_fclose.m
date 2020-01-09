cd97d6e2edbf6d3b2c1373cf3e2f8bcfa268e392
Chris Anderson from MET5Workstation <cnanderson@lbl.gov>
2020-01-09 12:14:56 -0800

    was in wrong directory beforfe so files did not get added.

---
+++ tests/test_fopen_fclose.m
@@ -0,0 +1,17 @@
+% textscan test
+
+dVals = rand(1, 100);
+dNum = length(dVals);
+
+dTic = tic;
+fid = fopen('test.csv', 'a'); 
+for m = 1 : dNum
+    fprintf(fid, '%1.5f', dVals(m));
+    if m < dNum
+        fprintf(fid, ',');
+    end
+end
+fclose(fid);
+dElapsedTime = toc(dTic);
+
+% commens
